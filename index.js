184a4c5dec4dedcd8042430e579d92d254bc6312
DiegoIgnacioCastroGarces <diegoignaciocg0@gmail.com>
2022-12-23 19:51:51 -0300

    Terminada la accion de puntuar

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-function score(btn) {
+function score() {
 
-    let clicked = document.getElementById(btn.id);
-    clicked.style.backgroundColor = "hsl(216, 12%, 54%)";
+    const allRatings = document.querySelectorAll('.ratingBtn');
+    
+    allRatings.forEach((rating, i) => {
+        rating.onclick = function() {
+            rating.style.backgroundColor = 'hsl(216, 12%, 54%)';
+            rating.style.color = 'hsl(0, 0%, 100%)';
 
+            let current_rating = i + 1;
+            
+            allRatings.forEach((rating, j) => {
+                if (current_rating != j + 1) {
+
+                    rating.style.backgroundColor = 'hsl(210, 19%, 18%)';
+                    rating.style.color = 'hsl(217, 12%, 63%)';
+                }
+            })
+
+            
+        }
+    })
     
 }
